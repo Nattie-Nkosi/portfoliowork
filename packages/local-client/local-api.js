e9f6faa533f11f3cb0952b8f3b5537ca1320bf82
Nutty Nkosi <nkosin361@gmail.com>
2021-09-27 12:46:15 +0200

    initial commit

---
+++ packages/local-client/local-api.js
@@ -0,0 +1,10 @@
+// import express
+
+// one route
+// serve up react production asserts
+
+// Save a list of cells into a file
+
+// Load up a list of cells from a file
+
+// express server listen on port 4050
